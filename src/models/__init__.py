3188b3fbc1766b64ca9aa41aa29947b5b75fe087
Kostov Ilya <kick1991@mail.ru>
2025-01-21 00:33:48 +0500

    feat: add model

---
+++ src/models/__init__.py
@@ -0,0 +1,7 @@
+__all__ = (
+    'ApplicationModel',
+    'BaseModel'
+)
+
+from src.models.base import BaseModel
+from src.models.application import ApplicationModel
---
+++ src/models/application.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.models import BaseModel
+from src.schemas.application import ApplicationSchemaDB, ApplicationResponse
+
+
+class ApplicationModel(BaseModel):
+    __tablename__ = 'applications'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_name: Mapped[str]
+    description: Mapped[str]
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+
+    def to_pydantic_schema(self) -> ApplicationResponse:
+        return ApplicationResponse(**self.__dict__)
